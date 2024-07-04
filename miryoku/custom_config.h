007d5cca1f27aaf4be14e836c92c8af5ef18240d
David <dadibom@hotmail.com>
2024-07-04 13:49:47 +0200

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,6 +2,7 @@
 #define XXX &none
 
 #define MIRYOKU_KLUDGE_MOUSEKEYSPR
+#define MIRYOKU_TAP_QWERTY
 
 #define MIRYOKU_LAYERMAPPING_TAP( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
@@ -10,9 +11,9 @@
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
 &kp ESC  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
+&kp LALT  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 &kp LSHFT  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &to U_BASE \
-               K32  K33  K34       K35  K36  K37
+               &kp LCTRL  &kp SPACE  &kp TAB       K35  K36  K37
 
 #define MIRYOKU_LAYERMAPPING_BASE( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
