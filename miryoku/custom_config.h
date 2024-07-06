e425a72feb71c22e29c9dad6a249c1b3baea1e85
David <dadibom@hotmail.com>
2024-07-06 15:54:06 +0200

    add play/pause button

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -10,7 +10,7 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp ESC  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
+&kp ESC  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp C_PLAY_PAUSE \
 &kp LALT  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 &kp LSHFT  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &to U_BASE \
                &kp LCTRL  &kp SPACE  &kp TAB       K35  K36  K37
@@ -21,7 +21,7 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
+XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp C_PLAY_PAUSE \
 XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 &to U_TAP  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
