3821da0322d9717f7b1bc8e68a8a087558c0c049
David <dadibom@hotmail.com>
2024-08-14 11:10:56 +0200

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -11,7 +11,7 @@
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
 &kp ESC  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp C_PLAY_PAUSE \
-&kp LALT  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp KC_LANG1 \
+&kp LALT  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp INTERNATIONAL_1 \
 &kp LSHFT  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &to U_BASE \
                &kp LCTRL  &kp SPACE  &kp TAB       K35  K36  K37
 
